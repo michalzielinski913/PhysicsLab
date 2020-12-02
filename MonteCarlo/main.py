a84e0b826df0cd6348bfc604ba4065efbf579e8f
Michał Zieliński <michalmaniak2000@gmail.com>
2020-12-02 13:17:02 +0100

    Monte Carlo
    Files used during PI calculation project

---
+++ MonteCarlo/main.py
@@ -0,0 +1,32 @@
+#Inspired by https://www.geeksforgeeks.org/estimating-value-pi-using-monte-carlo/
+#Changed points distribution to one quarter and added showProgress variable which can be used to check how program works in each iteration
+import random
+points = 100000
+showProgress= True
+circle_points = 0
+square_points = 0
+
+for i in range(points):
+
+    #We will work on <0, 1> range (one quarter of cirle)
+    rand_x = random.uniform(0, 1)
+    rand_y = random.uniform(0, 1)
+
+    #Check distance from points (0,0)
+    origin_dist = rand_x ** 2 + rand_y ** 2
+
+    #Check if points belong to circle
+    if origin_dist <= 1:
+        circle_points += 1
+    square_points += 1
+
+    #Calculate PI
+    pi = 4 * circle_points / square_points
+    #Debug option
+    if(showProgress):
+        print("Current pi estimation ", pi, (points-i), " points remaining")
+
+#Results
+print("Final Estimation of Pi=", pi)
+if (showProgress):
+    print(circle_points, " Circle points and ", square_points, " Square points")
---
+++ MonteCarlo/wykresy.py
@@ -0,0 +1,47 @@
+import numpy as np
+import plotly.express as px
+from sklearn.linear_model import LinearRegression
+import plotly.graph_objects as go
+x1 = np.array([7.4, 7.52, 8, 5.3, 4.86, 5.14, 25.84, 22.72, 46.08, 21.3])
+y1 = np.array([2.28, 2.52, 2.62, 1.68, 1.56, 1.52, 8.12, 7.16, 14.62, 6.52])
+ex = [0.109544512,
+0.074833148,
+0.063245553,
+0.063245553,
+0.048989795,
+0.048989795,
+0.621610811,
+0.074833148,
+0.074833148,
+0.089442719,
+]
+ey=[0.074833148,
+0.074833148,
+0.074833148,
+0.074833148,
+0.048989795,
+0.074833148,
+0.074833148,
+0.08,
+0.074833148,
+0.074833148,
+]
+#Converting x matrix in order to fit tensor
+xr=np.array(x1).reshape((-1, 1))
+yr=np.array(y1)
+#Passing x and y values to linear regression sklearn ML model
+# https://scikit-learn.org/stable/modules/linear_model.html
+model = LinearRegression().fit(xr, yr)
+#Generating 100 points in range <0, biggest measured x>
+x_range = np.linspace(0, xr.max(), 100)
+#Preditcting y values for 100 points given above
+y_range = model.predict(x_range.reshape(-1, 1))
+#coeficient is 1/Pi so we need to calculate (1/Pi)^-1 in order to get our result
+print("Predicted Pi value: ", model.coef_**-1)
+
+#Graph generation
+fig = px.scatter(x=x1, y=y1, error_x=ex, error_y=ey)
+fig.add_traces(go.Scatter(x=x_range, y=y_range, name='Linear regression'))
+fig.update_xaxes(showline=True, linewidth=2, linecolor='black', gridcolor='Grey', title_text='Circumference')
+fig.update_yaxes(showline=True, linewidth=2, linecolor='black', gridcolor='Grey', title_text='Diameter')
+fig.show()
